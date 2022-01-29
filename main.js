19dbde0a4a0098bdcb2f386010a2f384aefb866d
Siri20R <98664940+Siri20R@users.noreply.github.com>
2022-01-29 11:51:24 -0800

    This is my first copy!

---
+++ main.js
@@ -0,0 +1,7 @@
+function changecolor(){
+document.getElementById("para1").style.color="purple";
+document.getElementById("para2").style.color="#E040FB";
+document.getElementById("para3").style.color="#C62828";
+document.getElementById("para4").style.color="#00B8D4";
+document.getElementById("para5").style.color="#D81B60";
+}
